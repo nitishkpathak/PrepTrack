f213df6338018fa9625416bf31a716dc0200f322
Nitish pathak <npathak.sp@gmail.com>
2026-06-06 04:44:39 +0530

    fix(server): switch back to host smtp.gmail.com on port 587 to bypass port 465 blocks

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -2,7 +2,9 @@ const nodemailer = require("nodemailer");
 
 const sendEmail = async (options) => {
   const transporter = nodemailer.createTransport({
-    service: "gmail",
+    host: "smtp.gmail.com",
+    port: 587,
+    secure: false, // TLS port 587 requires secure: false
     auth: {
       user: process.env.EMAIL_USER,
       pass: process.env.EMAIL_PASS,
